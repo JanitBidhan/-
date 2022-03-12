5576060093a988c70a4aff1c922191f1275906a9
Janit Bidhan <janitbidhan@gmail.com>
2022-03-12 16:52:08 -0500

    Initial commit

---
+++ backend/src/test/java/edu/gmu/hackathon/rescue/RescueApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.gmu.hackathon.rescue;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RescueApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
